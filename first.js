b14cfee85736ad76892b7ad30e35104c5dca1ddb
briellaYourBae <briellayb@gmail.com>
2024-04-30 22:19:37 +0700

    first commit

---
+++ first.js
@@ -0,0 +1,27 @@
+//latiham pertama
+console.log("Hello All");
+console.log("XI " + "PPLG" + " 1");
+console.log("BriellaYB");
+
+//angka
+console.log(99 % 5);
+console.log(99 + 1 - 1);
+
+//TypeData
+let nama = "Abdul Aziz Taslim";
+console.log(nama);
+console.log("nama");
+
+//karakteristik
+let angka = 99;
+console.log(angka);
+
+angka = angka + 1;
+console.log(angka);
+
+let nick = "Abdul";
+console.log("kang " + nick);
+
+//update tanpa let/eksekusi
+nick = "chuaks";
+console.log(nick);
